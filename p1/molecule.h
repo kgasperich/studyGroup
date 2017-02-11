b06aae9ce829ac01e5b4d84f8777faf577c245fc
Kevin Gasperich <keg56@pitt.edu>
2017-02-10 20:17:11 -0500

    added more project1 stuff

--- p1/molecule.h
+++ p1/molecule.h
@@ -4,25 +4,44 @@
 #include <string>
 #include <algorithm>
 #include <Eigen/Dense>
+#include <iostream>
+#include <fstream>
+//#include <map>
+#include "symbols.h"
+#define SW(x) std::setw(x)
+using std::cout;
+using std::cerr;
+using std::endl;
 
 typedef Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> Mx3d;
+typedef Eigen::Matrix<double, 3, 3, Eigen::RowMajor> M33d;
 typedef Eigen::Matrix<double, 3, 1> V3d;
 
 class Molecule {
   public:
     int natom;
-    int charge;
+    int znuc;
+    int nelec;
+    double mass;
     std::vector<int> zvals;
+    std::vector<double> masses;
     std::vector<std::string> elnames;
     Mx3d xyz;
+    std::string comment;
 
+    M33d moi_tensor();
+    int charge();
     void print_xyz();
+    void print_zvals();
     void translate(V3d txyz);
+    V3d com();
     double bond(int n1, int n2);
     double angle(int n1, int n2, int n3);
-    double oop_angle(int n1, int n2, int n3, int n4);
-    double dihedral_angle(int n1, int n2, int n3, int n4);
-
+    double oop(int n1, int n2, int n3, int n4);
+    double dihedral(int n1, int n2, int n3, int n4);
+    void read_dotxyz(std::istream& is);
+    Molecule(const std::string& filename);
+    Molecule(int n, int q);
     Molecule();
     ~Molecule();
 };
--- p1/p1.cpp
+++ p1/p1.cpp
@@ -18,23 +18,21 @@
 #include <iomanip>
 #include <vector>
 #include <typeinfo>
-#include "symbols.h"
+//#include "symbols.h"
 #include "molecule.h"
 // Eigen matrix algebra library
 #include <Eigen/Dense>
-
+#define RMAX 4.0
 #define MAXBONDLENGTH 3.0
 using std::cout;
 using std::cerr;
 using std::endl;
 
-Eigen::IOFormat HeavyFmt(Eigen::FullPrecision, 0, ", ", ";\n", "[", "]", "[", "]");
-
-typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
-        Matrix;  // import dense, dynamically sized Matrix type from Eigen;
+//typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+//        Matrix;  // import dense, dynamically sized Matrix type from Eigen;
                  // this is a matrix with row-major storage (http://en.wikipedia.org/wiki/Row-major_order)
                  // to meet the layout of the integrals returned by the Libint integral library
-typedef Eigen::Matrix<double,3,1> V3d;
+//typedef Eigen::Matrix<double,3,1> V3d;
 struct Atom {
     int atomic_number;
     Eigen::Vector3d xyz;
@@ -54,17 +52,157 @@ std::vector<Atom> read_geometry(const std::string& filename, bool a2b);
 
 int main(int argc, char *argv[]) {
 
-  bool a2b = false;
   // read geometry from a file; by default read from h2o.xyz, else take filename (.xyz) from the command line
   const auto filename = (argc > 1) ? argv[1] : "acetaldehyde.xyz";
-  std::vector<Atom> atoms = read_geometry(filename,a2b);
 
   // count the number of electrons
-  auto nelectron = 0;
-  for (auto i = 0; i < atoms.size(); ++i)
-    nelectron += atoms[i].atomic_number;
-  const auto ndocc = nelectron / 2;
-  
+//  auto nelectron = 0;
+//  for (auto i = 0; i < atoms.size(); ++i)
+//    nelectron += atoms[i].atomic_number;
+//  const auto ndocc = nelectron / 2;
+  Molecule mol(filename);
+  mol.print_xyz();
+
+  cout << "\nBond lengths:" << endl;
+  for (auto i=0; i < mol.natom; i++) {
+    for (auto j=0; j < i; j++) {
+      cout << "r " << i << " " << j << "  " << mol.bond(i,j) << endl;
+    }
+  }
+
+  cout << "\nBond angles (correct):" << endl;
+  for (auto i = 0; i < mol.natom; i++)
+    for (auto j = 0; j < mol.natom; j++) {
+      if (j != i) {
+        if (mol.bond(i,j) < RMAX) {
+          for (auto k = 0; k < i; k++) {
+            if (k != j) {
+              if (mol.bond(j,k) < RMAX) {
+                cout << "φ " << i << " " << j << " "<< k << "  " << mol.angle(i,j,k) << endl;
+              }
+            }
+          }
+        }
+      }
+    }
+  cout << "\nBond angles (Crawford):" << endl;
+  for (auto i = 0; i < mol.natom; i++)
+    for (auto j = 0; j < i; j++) {
+      if (mol.bond(i,j) < 4.0) {
+        for (auto k = 0; k < j; k++) {
+          if (mol.bond(j,k) < 4.0) {
+            cout << "φ " << i << " " << j << " "<< k << "  " << mol.angle(i,j,k) << endl;
+          }
+        }
+      }
+    }
+
+  cout << "\nOOP angles:" << endl;
+  for (auto k = 0; k < mol.natom; k++)
+    for (auto i = 0; i < mol.natom; i++) {
+      if (i != k) {
+        if (mol.bond(i,k) < RMAX) {
+          for (auto j = 0; j < i; j++) {
+            if (j != k) {
+              if (mol.bond(j,k) < RMAX) {
+                for (auto l = 0; l < mol.natom; l++) {
+                  if ((l != k) && (l != i) && (l != j)) {
+                    if (mol.bond(k,l) < RMAX) {
+                      cout << "θ " 
+                        << i << " " << j << " " << k << " " << l << "  " 
+                        << mol.oop(i,j,k,l) << endl;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+  cout << "\nOOP angles (Crawford):" << endl;
+  for (auto i = 0; i < mol.natom; i++)
+    for (auto k = 0; k < mol.natom; k++) {
+      if (i != k) {
+        if (mol.bond(i,k) < RMAX) {
+          for (auto j = 0; j < mol.natom; j++) {
+            if ((j != k) && (j != i)) {
+              if (mol.bond(j,k) < RMAX) {
+                for (auto l = 0; l < j; l++) {
+                  if ((l != k) && (l != i)) {
+                    if (mol.bond(k,l) < RMAX) {
+                      cout << "θ " 
+                        << i << " " << j << " " << k << " " << l << "  " 
+                        << SW(10) << mol.oop(i,j,k,l) << endl;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+  cout << "\ndihedral angles:" << endl;
+  for (auto j = 0; j < mol.natom; j++)
+    for (auto k = 0; k < j; k++) {
+      if (k != j) {
+        if (mol.bond(j,k) < RMAX) {
+          for (auto i = 0; i < mol.natom; i++) {
+            if ((i != j) && (i != k)) {
+              if (mol.bond(j,i) < RMAX) {
+                for (auto l = 0; l < mol.natom; l++) {
+                  if ((l != k) && (l != i) && (l != j)) {
+                    if (mol.bond(k,l) < RMAX) {
+                      cout << "τ "
+                        << i << " " 
+                        << j << " " 
+                        << k << " " 
+                        << l << "  " << SW(10) << mol.dihedral(i,j,k,l) << endl;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+  cout << "\ndihedral angles (Crawford):" << endl;
+  for (auto i = 0; i < mol.natom; i++)
+    for (auto j = 0; j < i; j++) {
+      if (mol.bond(j,i) < RMAX) {
+        for (auto k = 0; k < j; k++) {
+          if (mol.bond(j,k) < RMAX) {
+            for (auto l = 0; l < k; l++) {
+              if (mol.bond(k,l) < RMAX) {
+                cout << "τ "
+                  << i << " " 
+                  << j << " " 
+                  << k << " " 
+                  << l << "  " << SW(10) << mol.dihedral(i,j,k,l) << endl;
+              }
+            }
+          }
+        }
+      }
+    }
+
+  cout << "\ncenter of mass:" << endl;
+  auto com = mol.com();
+  cout << com.transpose() << endl;
+  mol.translate(-com);
+  com = mol.com();
+  cout << com.transpose() << endl;
+
+//  auto r1 = mol.xyz.row(1);
+//  cout << r1 << endl;
+//  cout << typeid(r1).name() << endl;
+//  cout << r1.norm() << endl;
+/*  
   cout << atoms[5].xyz.array() * atoms[6].xyz.array() << endl;
   print_bond_lengths(atoms);
   print_bond_angles(atoms);
@@ -75,7 +213,7 @@ int main(int argc, char *argv[]) {
   cout << txyz.transpose() << endl;
   translate(atoms, -1*txyz);
   print_xyz(atoms);
-
+*/
 /*
   auto r1 = atoms[1].xyz;
   V3d r1b = atoms[1].xyz;
@@ -88,144 +226,6 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-std::vector<Atom> read_dotxyz(std::istream& is, bool a2b) {
-  // line 1 = # of atoms
-  size_t natom;
-  is >> natom;
-  // read off the rest of line 1 and discard
-  std::string rest_of_line;
-  std::getline(is, rest_of_line);
-
-  // line 2 = comment (possibly empty)
-  std::string comment;
-  std::getline(is, comment);
-
-  std::vector<Atom> atoms(natom);
-  for (auto i = 0; i < natom; i++) {
-    std::string element_symbol;
-    double x, y, z;
-    is >> element_symbol >> x >> y >> z;
-
-    int Z;
-    // if given as numeric value, convert to int
-    if (isdigit(element_symbol[0])) {
-      Z = stoi(element_symbol);
-    } else {
-    // if given as symbol, use map to get value
-      try {
-        Z = symbolToZ.at(element_symbol);
-      } catch (std::exception& oe) {
-        cerr << "\nError: element " << element_symbol << " not recognized\n" << oe.what() << endl;
-        throw "Did not recognize element symbol in .xyz file";
-      }
-    }
-  
-    atoms[i].atomic_number = Z;
-
-    // .xyz files report Cartesian coordinates in angstroms; convert to bohr
-
-    atoms[i].xyz << x , y , z;
-    const auto angstrom_to_bohr = 1 / 0.52917721092; // 2010 CODATA value
-    if(a2b){atoms[i].xyz *= angstrom_to_bohr;}
-  }
-  return atoms;
-}
-
-std::vector<Atom> read_geometry(const std::string& filename, bool a2b) {
-
-  std::cout << "Will read geometry from " << filename << std::endl;
-  std::ifstream is(filename);
-  assert(is.good());
-
-  // to prepare for MPI parallelization, we will read the entire file into a string that can be
-  // broadcast to everyone, then converted to an std::istringstream object that can be used just like std::ifstream
-  std::ostringstream oss;
-  oss << is.rdbuf();
-  // use ss.str() to get the entire contents of the file as an std::string
-  // broadcast
-  // then make an std::istringstream in each process
-  std::istringstream iss(oss.str());
-
-  // check the extension: if .xyz, assume the standard XYZ format, otherwise throw an exception
-  if ( filename.rfind(".xyz") != std::string::npos)
-    return read_dotxyz(iss, a2b);
-  else
-    throw "only .xyz files are accepted";
-}
-
-void print_bond_lengths(const std::vector<Atom> mol, double rmax) {
-  auto enuc = 0.0;
-  for (auto i = 0; i < mol.size(); i++)
-    for (auto j = 0; j < i; j++) {
-      auto rij = mol[i].xyz - mol[j].xyz;
-      auto r = rij.norm();
-      enuc += mol[i].atomic_number * mol[j].atomic_number / r;
-      if (r < rmax) {
-        cout << "r" << i << j << " = " << r << endl;
-      }
-    }
-  cout << "E_nuc = " << enuc << endl;
-}
-
-void print_bond_angles(const std::vector<Atom> mol, double rmax) {
-  auto N = mol.size();
-  for (auto i = 0; i < N; i++)
-    for (auto j = 0; j < N; j++) {
-      if (j != i) {
-// can't use auto here if rji is to be normalized?
-//        auto rji = mol[i].xyz - mol[j].xyz;
-        V3d rji = mol[i].xyz - mol[j].xyz;
-        auto dji = rji.norm();
-        if (dji < rmax) {
-          rji.normalize();
-          for (auto k = 0; k < i; k++) {
-            if (k != j) {
-//              auto rjk = mol[k].xyz - mol[j].xyz;
-              V3d rjk = mol[k].xyz - mol[j].xyz;
-              auto djk = rjk.norm();
-              if (djk < rmax) {
-                rjk.normalize();
-                cout << "φ" << i << j << k << " = " << bond_angle(rji,rjk) << endl;
-              }
-            }
-          }
-        }
-      }
-    }
-}
-
-void print_oop_angles(const std::vector<Atom> mol, double rmax) {
-  auto N = mol.size();
-  for (auto k = 0; k < N; k++)
-    for (auto i = 0; i < N; i++) {
-      if (i != k) {
-        V3d rki = mol[i].xyz - mol[k].xyz;
-        auto dki = rki.norm();
-        if (dki < rmax) {
-          rki.normalize();
-          for (auto j = 0; j < i; j++) {
-            if (j != k) {
-              V3d rkj = mol[j].xyz - mol[k].xyz;
-              auto dkj = rkj.norm();
-              if (dkj < rmax) {
-                rkj.normalize();
-                for (auto l = 0; l < N; l++) {
-                  if ((l != k) && (l != i) && (l != j)) {
-                    V3d rkl = mol[l].xyz - mol[k].xyz;
-                    auto dkl = rkl.norm();
-                    if (dkl < rmax) {
-                      rkl.normalize();
-                      cout << "θ" << i << j << k << l << " = " << oop_angle(rki,rkj,rkl) << endl;
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-}
 
 void print_dihedral_angles(const std::vector<Atom> mol, double rmax) {
   auto N = mol.size();
@@ -261,7 +261,7 @@ V3d center_of_mass(const std::vector<Atom> mol) {
   V3d xyzcom = V3d::Zero();
   auto mtot = 0.0;
   for (auto i = 0; i < mol.size(); i++) {
-    auto mi = atomicMass[mol[i].atomic_number];
+    auto mi = atomicMassIso[mol[i].atomic_number];
     xyzcom += mi * mol[i].xyz;
     mtot += mi;
   }
@@ -273,36 +273,6 @@ void translate(std::vector<Atom> &mol, V3d txyz) {
     mol[i].xyz += txyz;
   }
 }
-
-void print_xyz(const std::vector<Atom> mol) {
-  cout.precision(4);
-  for (auto i = 0; i < mol.size(); i++) {
-    //cout << mol[i].atomic_number << "\t" << mol[i].xyz.transpose().format(HeavyFmt) << endl;
-    cout << mol[i].atomic_number << "\t" << mol[i].xyz.transpose() << endl;
-  }
-}
-double bond_angle(const Eigen::Vector3d rji, const Eigen::Vector3d rjk, bool normed) {
-  auto jidotjk = rji.dot(rjk);
-  if (normed) {
-    return acos(jidotjk) * 180.0 / M_PI; 
-  } else {
-    auto jijk = rji.norm()*rjk.norm();
-    return acos(jidotjk/jijk) * 180.0 / M_PI; 
-  }
-}
-
-double oop_angle(const Eigen::Vector3d rki, const Eigen::Vector3d rkj, const Eigen::Vector3d rkl, bool normed) {
-  auto kjcrosskl = rkj.cross(rkl);
-  kjcrosskl.normalize();
-  auto num = kjcrosskl.dot(rki);
-  if (normed) {
-    return asin(num) * 180.0 / M_PI; 
-  } else {
-    auto dki = rki.norm();
-    return asin(num/dki) * 180.0 / M_PI; 
-  }
-}
-
 
 double dihedral_angle(const Eigen::Vector3d rji,const Eigen::Vector3d rjk,const Eigen::Vector3d rkl) {
   //TODO: compare n1 x n2 to rjk to determine sign of angle
